d3e2389bbd9df902531da954af99c4fdbddf59bc
mgorriz <mgorriz@joyUbuntu>
2017-07-16 15:00:08 +0800

    Sudoku field is  ready

--- Sudoku/src/ViewGUI.java
+++ Sudoku/src/ViewGUI.java
@@ -1,50 +1,114 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.Component.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JFormattedTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.InputMethodListener;
-import java.util.ArrayList;
-import java.awt.event.InputMethodEvent;
+import java.awt.Font;
 
+@SuppressWarnings("serial")
 public class ViewGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel squaresPane;
+	
+	
+	private JFormattedTextField[] jField = new JFormattedTextField[81];
+	private final static int POSSTART = 30;
+	private final static int BOXWIDTH = 40;
+	private final static int BOXGAP = 0;
 
+	private class SquaresPanel extends JPanel {
+
+		SquaresPanel() {
+	        this.setOpaque(false);
+			this.setSize(420, 500);
+			this.setVisible(true);
+	        }
+
+		@Override
+		public void paintComponent(Graphics g) {
+			Graphics2D g2= (Graphics2D) g;
+			super.paintComponent(g2);
+
+			final int LINEGAP = 0;
+			
+			for (int i=0;i<81;i++) {
+				g2.drawRect(POSSTART+ (i%9)*(BOXWIDTH+BOXGAP), 
+							POSSTART+ (i/9)*(BOXWIDTH+BOXGAP), 
+							BOXWIDTH, BOXWIDTH);
+			};
+			
+			g2.setStroke (new BasicStroke (3));
+			
+			for(int i=0;i<9;i++) {
+				g2.drawRect(POSSTART+ (i%3)*3*(BOXWIDTH+BOXGAP), 
+							POSSTART+ (i/3)*3*(BOXWIDTH+BOXGAP), 
+							3*BOXWIDTH, 3*BOXWIDTH);
+			};
+						
+		}
+		
+	}
 
 	public ViewGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 470, 384);
+		setBounds(0, 0, 420, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		
+		JButton btnStore = new JButton("Store Values");
+		btnStore.setFont(new Font("Dialog", Font.PLAIN, 18));
+		btnStore.setBounds(POSSTART, 440, 160, BOXWIDTH);
+		btnStore.setOpaque(false);
+		btnStore.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JButton btnSolve = new JButton("Solve");
+		btnSolve.setFont(new Font("Dialog", Font.PLAIN, 18));
+		btnSolve.setBounds(POSSTART+160+BOXWIDTH, 440, 160, BOXWIDTH);
+		btnSolve.setOpaque(false);
+		btnSolve.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
 		contentPane.setLayout(null);
+		contentPane.add(btnSolve);
+		contentPane.add(btnStore);
+		contentPane.setVisible(true);
+
+		squaresPane = new SquaresPanel();
+		contentPane.add(squaresPane);
+
+		
 				
+		for (int i=0;i<81;i++) {
+			jField[i] = new JFormattedTextField();
+			jField[i].setFont(new Font("Dialog", Font.PLAIN, 24));
+			jField[i].setName("R"+Integer.toString(i%9+1)+"C"+Integer.toString(i/9));
+			jField[i].setBounds(POSSTART+ (i%9)*(BOXWIDTH+BOXGAP), 
+								POSSTART+ (i/9)*(BOXWIDTH+BOXGAP), 
+								BOXWIDTH, BOXWIDTH);
+			contentPane.add(jField[i]);
+			contentPane.setLayout(null);
+		}
 		
-		JFormattedTextField formattedTextField_1 = new JFormattedTextField();
-		formattedTextField_1.setText("1");
-		formattedTextField_1.setName("R1C1");
-		formattedTextField_1.setBounds(27, 28, 22, 19);
-		contentPane.add(formattedTextField_1);
-		
-		
-		JFormattedTextField formattedTextField = new JFormattedTextField();
-		formattedTextField.setText("2");
-		formattedTextField.setName("R1C2");
-		formattedTextField.setBounds(50, 28, 22, 19);
-		contentPane.add(formattedTextField);
-		
-		JFormattedTextField formattedTextField_2 = new JFormattedTextField();
-		formattedTextField_2.setText("3");
-		formattedTextField_2.setName("R1C3");
-		formattedTextField_2.setBounds(71, 28, 22, 19);
-		contentPane.add(formattedTextField_2);
+
 	}
+
 }
+
+
+
+
